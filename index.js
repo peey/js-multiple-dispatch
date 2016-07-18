aab671006f553a5f0e4949b96f6ff4e6c43177e2
Peeyush Kushwaha <peeyush.p97@gmail.com>
2016-07-18 20:56:21 +0530

    implement strict, warning, and first-method modes for call resolver (without tests)

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-var assert = require("assert")
-
-var GenericFunction = function (docstring) {
+var GenericFunction = function (docstring, callResolverIdentifier) {
   this.methods = []
   this.docstring = docstring
+  this.callResolverIdentifier = callResolverIdentifier
 }
 
 GenericFunction.prototype.call = function (context, args) {
-  var found = this.methods.find((method) => method.check.apply(context, args))
-  if (found) {
-    return found.executor.apply(context, args)
+  var mathchingMethods = this.methods.filter((method) => method.check.apply(context, args))
+  return this.callResolver(mathchingMethods, context, args)
+}
+
+GenericFunction.prototype.defmethod = function (check, executor) {
+  return this.methods.push({check, executor})
+}
+
+GenericFunction.prototype.firstMethodCallResolver = function (matchingMethods, context, args) {
+  if (matchingMethods.length >= 1) {
+    return matchingMethods[0].executor.apply(context, args)
   } else {
     throw new ReferenceError("call to generic function with docstring '" + this.docstring + "': no matching method found for the arguments " + args.toString())
   }
 }
 
-GenericFunction.prototype.defmethod = function (check, executor) {
-  return this.methods.push({check, executor})
+GenericFunction.prototype.warningCallResolver = function (matchingMethods, context, args) {
+  if (matchingMethods.length > 1) {
+    console.warn("generic function warning: more than one methods match the given arguments " + args.toString() + ", arbitarily selecting the first one")
+  }
+  return this.firstMethodCallResolver(matchingMethods, context, args)
+}
+
+GenericFunction.prototype.strictCallResolver = function (matchingMethods, context, args) {
+  if (matchingMethods.length > 1) {
+    throw new RangeError("generic function does not have discreetely partitioned domain, multiple methods match the given arguments " + args.toString())
+  } else {
+    return this.defaultCallResolver(matchingMethods, context, args)
+  }
+}
+
+GenericFunction.prototype.callResolver = function (matchingMethods, context, args) {
+  switch (this.callResolverIdentifier) {
+    case "first-method": return this.firstMethodCallResolver(matchingMethods, context, args)
+    case "strict": return this.strictCallResolver(matchingMethods, context, args)
+    default: return this.warningCallResolver(matchingMethods, context, args)
+  }
 }
 
 var defgeneric = function (docstring) {

5bd62d03f5fa6ce4b2304152e530707b06a34781
Peeyush Kushwaha <peeyush.p97@gmail.com>
2016-07-18 22:47:55 +0530

    some renaming; tests related to method domain overlapping added, not passing yet

--- index.js
+++ index.js
@@ -4,51 +4,55 @@ var GenericFunction = function (docstring, callResolverIdentifier) {
   this.callResolverIdentifier = callResolverIdentifier
 }
 
-GenericFunction.prototype.call = function (context, args) {
-  var mathchingMethods = this.methods.filter((method) => method.check.apply(context, args))
-  return this.callResolver(mathchingMethods, context, args)
+GenericFunction.prototype.call = function (methodCallContext, methodCallArgs) {
+  var mathchingMethods = this.methods.filter((method) => method.check.apply(methodCallContext, methodCallArgs))
+  return this.callResolver(mathchingMethods, methodCallContext, methodCallArgs)
 }
 
 GenericFunction.prototype.defmethod = function (check, executor) {
   return this.methods.push({check, executor})
 }
 
-GenericFunction.prototype.firstMethodCallResolver = function (matchingMethods, context, args) {
+GenericFunction.prototype.firstMethodCallResolver = function (matchingMethods, methodCallContext, methodCallArgs) {
   if (matchingMethods.length >= 1) {
-    return matchingMethods[0].executor.apply(context, args)
+    return matchingMethods[0].executor.apply(methodCallContext, methodCallArgs)
   } else {
-    throw new ReferenceError("call to generic function with docstring '" + this.docstring + "': no matching method found for the arguments " + args.toString())
+    throw new ReferenceError("call to generic function with docstring '" + this.docstring + "': no matching method found for the arguments " + methodCallArgs.toString())
   }
 }
 
-GenericFunction.prototype.warningCallResolver = function (matchingMethods, context, args) {
+GenericFunction.prototype.warningCallResolver = function (matchingMethods, methodCallContext, methodCallArgs) {
   if (matchingMethods.length > 1) {
-    console.warn("generic function warning: more than one methods match the given arguments " + args.toString() + ", arbitarily selecting the first one")
+    console.warn("generic function warning: more than one methods match the given arguments " + methodCallArgs.toString() + ", arbitarily selecting the first one")
   }
-  return this.firstMethodCallResolver(matchingMethods, context, args)
+  return this.firstMethodCallResolver(matchingMethods, methodCallContext, methodCallArgs)
 }
 
-GenericFunction.prototype.strictCallResolver = function (matchingMethods, context, args) {
+GenericFunction.prototype.strictCallResolver = function (matchingMethods, methodCallContext, methodCallArgs) {
   if (matchingMethods.length > 1) {
-    throw new RangeError("generic function does not have discreetely partitioned domain, multiple methods match the given arguments " + args.toString())
+    throw new RangeError("generic function does not have discreetely partitioned domain, multiple methods match the given arguments " + methodCallArgs.toString())
   } else {
-    return this.defaultCallResolver(matchingMethods, context, args)
+    return this.defaultCallResolver(matchingMethods, methodCallContext, methodCallArgs)
   }
 }
 
-GenericFunction.prototype.callResolver = function (matchingMethods, context, args) {
+GenericFunction.prototype.callResolver = function (matchingMethods, methodCallContext, methodCallArgs) {
+  //this "resolution" is what I've termed the act of determining what to do after check functions have been run
+  //i.e. you might want to throw an error on domain overlap / arguments satisfying multiple methods' check functions
+  //or emit warning (default behavior)
+  //or do nothing and just run the first found method
   switch (this.callResolverIdentifier) {
-    case "first-method": return this.firstMethodCallResolver(matchingMethods, context, args)
-    case "strict": return this.strictCallResolver(matchingMethods, context, args)
-    default: return this.warningCallResolver(matchingMethods, context, args)
+    case "first-method": return this.firstMethodCallResolver(matchingMethods, methodCallContext, methodCallArgs)
+    case "strict": return this.strictCallResolver(matchingMethods, methodCallContext, methodCallArgs)
+    default: return this.warningCallResolver(matchingMethods, methodCallContext, methodCallArgs)
   }
 }
 
 var defgeneric = function (docstring) {
   var genericFunction = new GenericFunction(docstring) //has-a relationship
   
-  var call = function (...args) {
-    return genericFunction.call(this, args)
+  var call = function (...methodCallArgs) {
+    return genericFunction.call(this, methodCallArgs)
   }
   
   return Object.assign(call, {
--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
 var assert = require("assert")
 var _ = require("lodash")
 var defgeneric = require("../index.js")
-
+var sinon = require("sinon")
+require("mocha-sinon")
 describe("js-multiple-dispatch", function () {
   it("should return a function", function () {
     var add = defgeneric("testing")
@@ -77,6 +78,85 @@ describe("js-multiple-dispatch", function () {
 
       assert(add(1, 2) == 3)
       assert(add([1,2,3]) == 6)
+    })
+  })
+  
+  describe("method domain overlapping", function () {
+    it("should emit warning (by default) when arguments satisfy check functions of multiple methods", function () {
+      //hack: http://stackoverflow.com/a/30626035/1412255 aka something I don't understand
+      var stub = this.sinon.stub(console, 'warn')
+      var test = defgeneric("testing")
+    
+      test
+        .defmethod(
+        (a) => a > 3,
+        function (a) {
+          return a
+        })
+        .defmethod(
+        (a) => a < 5,
+        function (a) {
+          return a
+        })
+      
+      test(4)
+      assert(console.warn.callCount === 1)
+      
+      test(6)
+      assert(console.warn.callCount === 1)
+    })
+    
+    it("should throw error in strict mode when domain overlaps", function () {
+      var test = defgeneric("testing", "strict")
+    
+      test
+        .defmethod(
+        (a) => a > 3,
+        function (a) {
+          return a
+        })
+        .defmethod(
+        (a) => a < 5,
+        function (a) {
+          return a
+        })
+      
+      var threw = false
+      try {
+        test(4)
+      } catch (e) {
+        threw = true
+      }
+      
+      assert(threw === true)
+      assert(test(6) === 6) //no throw
+    })
+    
+    it("shouldn't throw error or emit warning in first-method mode", function () {
+      var stub = this.sinon.stub(console, 'warn')
+      var test = defgeneric("testing", "first-method")
+      
+      test
+        .defmethod(
+        (a) => a > 3,
+        function (a) {
+          return a
+        })
+        .defmethod(
+        (a) => a < 5,
+        function (a) {
+          return a
+        })
+      
+      var threw = false
+      try {
+        test(4)
+      } catch (e) {
+        threw = true
+      }
+      
+      assert(threw === false)
+      assert(console.warn.callCount === 0)
     })
   })
 })
